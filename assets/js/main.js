ecedbd52338d7fc0ec9c4dcad369065e08b86fa5
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-20 15:34:27 +0200

    User sceglie un numero

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
-alert("funziona")
-
 /*L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata,
 in cui ogni cella contiene un numero tra quelli compresi in un range:
 con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.*/
+
+
+//L'utente indica un livello di difficoltà
+const user_choice_difficult = parseInt(prompt("Inserisci un livello di difficoltà compreso tra 1 e 3:"));
+//console.log(user_choice_difficult);
+
+
