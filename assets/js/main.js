e6dba8e71714afa27b05ed90bebf47fc66d54b0c
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-20 15:51:50 +0200

    aggiungo i numeri dentro le celle

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,7 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.*/
 const user_choice_difficult = parseInt(prompt("Inserisci un livello di difficoltà compreso tra 1 e 3:"));
 //console.log(user_choice_difficult);
 
+//In base alla scelta do una dimensione alla griglia
 let cell_number;
 if (user_choice_difficult == 1) {
     cell_number = 100;
@@ -23,16 +24,19 @@ if (user_choice_difficult == 1) {
 
 const containerElement = document.querySelector(".container")
 
+//Creo la griglia cliccabile in base al valore passato
 function createGridClick(cell_number) {
-    for (let i = 0; i < cell_number; i++) {
+    for (let i = 1; i <= cell_number; i++) {
         let gridElement = document.createElement("div");
         gridElement.className = "grid_cell"
+        gridElement.innerHTML = i
         containerElement.append(gridElement)
 
         gridElement.addEventListener("click", function () {
             //console.log(this);
 
             this.style.backgroundColor = "#99CBFF"
+            this.style.color = "black"
         })
 
     }
